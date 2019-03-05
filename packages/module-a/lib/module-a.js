e5953174f232d5ccc0e78af1ec7f49c7cf7f1f35
yangjian <yangjian@megvii.com>
2019-03-05 13:15:09 +0800

    init

---
+++ packages/module-a/lib/module-a.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const moduleB = require('module-b');
+
+const moduleA = function() {
+    console.log(moduleB.sayHello());
+}
+
+export default moduleA;
---
+++ packages/module-b/lib/module-b.js
@@ -0,0 +1,5 @@
+'use strict';
+
+export const sayHello() {
+    return "hello world";
+};
